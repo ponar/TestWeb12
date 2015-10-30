bc7b4c2c1b427a6e9578ad4d566532fbcc4190ed
Jarek Zyzak <jarekpc@o2.pl>
2015-10-30 14:16:10 +0100

    inicjalizacja projektu

---
+++ src/main/java/com/example/TestWeb12Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWeb12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestWeb12Application.class, args);
+    }
+}
