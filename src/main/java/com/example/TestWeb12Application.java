d9e1f8cfd676f05915b34ca45ed305f9fb177916
Jarek Zyzak <jarekpc@o2.pl>
2015-10-30 14:33:48 +0100

    Poprawa źródła

--- src/main/java/com/example/TestWeb12Application.java
+++ src/main/java/com/example/TestWeb12Application.java
@@ -2,10 +2,21 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class TestWeb12Application {
-
+	
+	@RequestMapping("/")
+	String home() {
+	        return "Hello World!";
+	    }
+	 
     public static void main(String[] args) {
         SpringApplication.run(TestWeb12Application.class, args);
     }
